bfde4ab93f2442e6f548fd868063ce5732e87c4d
AdminSena <AdminSena@GARAPRCADFSD616.FormacionSena.RED>
2023-08-18 19:34:06 -0500

    subo proyecto con rutas creadas

---
+++ app.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+
+const app = express();
+
+
+app.use(morgan('dev'));
+
+
+app.use(express.static('public'));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => {
+  res.render('inicio');
+});
+
+app.get('/atencion_al_cliente', (req, res) => {
+  res.render('atencion_al_cliente');
+});
+
+app.get('/nosotros', (req, res) => {
+  res.render('nosotros');
+});
+
+app.get('/testimonio', (req, res) => {
+  res.render('testimonio');
+});
+
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor iniciado en http://localhost:${port}`);
+});
